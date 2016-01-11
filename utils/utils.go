08e5f942082469ea1abefdd8ec7c67cbad819472
Thomas Cyron <thomas@thcyron.de>
2016-01-11 20:48:14 +0100

    Remove unused Haversine func

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,6 @@
 package utils
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 type Duration uint // seconds
 
@@ -50,18 +47,4 @@ func (p Pace) String() string {
 	min := int(secPerKM / 60)
 	sec := int(secPerKM) % 60
 	return fmt.Sprintf("%d:%02d min/km", min, sec)
-}
-
-const earthRadius = 6371000
-
-func Haversine(lat1, lon1, lat2, lon2 float64) float64 {
-	dLat := (lat2 - lat1) * (math.Pi / 180.0)
-	dLon := (lon2 - lon1) * (math.Pi / 180.0)
-	rlat1 := lat1 * (math.Pi / 180.0)
-	rlat2 := lat2 * (math.Pi / 180.0)
-	a1 := math.Sin(dLat/2) * math.Sin(dLat/2)
-	a2 := math.Sin(dLon/2) * math.Sin(dLon/2) * math.Cos(rlat1) * math.Cos(rlat2)
-	a := a1 + a2
-	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
-	return earthRadius * c
 }
