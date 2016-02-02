03cd2b3fc88b124c03c5945792c90c627c2e648c
Thomas Cyron <thomas@thcyron.de>
2016-02-02 23:29:22 +0100

    Use Highcharts for heartrate pie chart
    Closes #10.

--- js/components/LogHeartrateZones.js
+++ js/components/LogHeartrateZones.js
@@ -1,48 +1,74 @@
 "use strict";
 
 import React from "react";
-import Chart from "chart.js";
+import Highcharts from "highcharts";
 
 export default class LogHeartrateZones extends React.Component {
-  _createChart(canvas) {
-    if (canvas != null) {
-      new Chart(canvas.getContext("2d")).Doughnut(this.data, {
-        animation: false,
-        tooltipTemplate: "<%= label %>: <%= value %>%",
-      });
+ _createChart(container) {
+    if (container == null) {
+      return;
     }
+    Highcharts.chart(container, {
+      chart: {
+        type: "pie",
+        style: {
+          fontFamily: `"Helvetica Neue", Helvetica, Arial, sans-serif`,
+          fontSize: "12px",
+        },
+      },
+      title: {
+        text: null,
+      },
+      tooltip: {
+        pointFormat: "<b>{point.y}</b>",
+        valueSuffix: "%",
+      },
+      legend: {
+        enabled: false,
+      },
+      series: [
+        {
+          name: "Heartrate",
+          color: "rgb(30, 179, 0)",
+          data: this.data,
+          dataLabels: {
+            enabled: false,
+          },
+        },
+      ],
+    });
   }
 
   get data() {
     return [
       {
-        value: Math.round(this.props.zones.get("red")),
-        label: "Red",
+        y: Math.round(this.props.zones.get("red")),
+        name: "Red",
         color: "#d9534f",
       },
       {
-        value: Math.round(this.props.zones.get("anaerobic")),
-        label: "Anaerobic",
+        y: Math.round(this.props.zones.get("anaerobic")),
+        name: "Anaerobic",
         color: "#f0ad4e",
       },
       {
-        value: Math.round(this.props.zones.get("aerobic")),
-        label: "Aerobic",
+        y: Math.round(this.props.zones.get("aerobic")),
+        name: "Aerobic",
         color: "#80c780",
       },
       {
-        value: Math.round(this.props.zones.get("fatburning")),
-        label: "Fat Burning",
+        y: Math.round(this.props.zones.get("fatburning")),
+        name: "Fat Burning",
         color: "#449d44",
       },
       {
-        value: Math.round(this.props.zones.get("easy")),
-        label: "Easy",
+        y: Math.round(this.props.zones.get("easy")),
+        name: "Easy",
         color: "#5bc0de",
       },
       {
-        value: Math.round(this.props.zones.get("none")),
-        label: "None",
+        y: Math.round(this.props.zones.get("none")),
+        name: "None",
         color: "#777777",
       },
     ];
@@ -55,7 +81,7 @@ export default class LogHeartrateZones extends React.Component {
           <h4 className="panel-title">Heart Rate Zones</h4>
         </div>
         <div className="panel-body text-center">
-          <canvas width="220" height="220" ref={this._createChart.bind(this)}></canvas>
+          <div style={{height: "230px"}} ref={this._createChart.bind(this)}></div>
         </div>
       </div>
     );
